e62f8959cc522852d44747ca600acae5d097ec7b
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-10-25 15:12:59 +0300

    Completed Lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+require "pry"
 
+class Book
+    # Reader method short cut.
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count
+
+    # This is the setter method
+    def initialize(title)
+        @title = title
+    end
+
+    #THis is a reader long method
+    # def title
+    #     @title
+    # end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+
+# binding.pry
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+require "pry"
+
+class Shoe
+    # reader/writer method short cuts.
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    # This is the setter method
+    def initialize(brand)
+        @brand = brand
+    end
+    
+    def cobble
+        #updsting the condition of the shue
+        @condition ="new"
+        #returning the message
+       puts "Your shoe is as good as new!"
+    end
+end
+
